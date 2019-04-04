0678d5afba034a34c7053ad84705bfb7ed0b8c06
MobbareKurtZ <hannes.skoog@gmail.com>
2019-04-04 10:59:45 +0200

    auth test

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,29 @@
 <template>
   <div id="app">
     <h1>Spotivue</h1>
+    <button @click="auth()">test</button>
   </div>
 </template>
 
 <script>
-
 export default {
-  name: 'spotivue',
-  components: {
+  name: "spotivue",
+  components: {},
+  methods: {
+    async auth() {
+      let baseUrl = "https://accounts.spotify.com/authorize";
+      let req = {
+        client_id: "115a0fafb3a7416b8dce3989aaba0653",
+        response_type: "code",
+        redirect_uri: "localhost:8080"
+      };
+      let res = await this.$http.get(baseUrl, { params: req });
+      console.log(res);
+    }
   }
-}
+};
 </script>
 
 <style lang="scss">
-@import "@/scss/main.scss"
+@import "@/scss/main.scss";
 </style>
