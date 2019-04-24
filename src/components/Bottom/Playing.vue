1bc31e9657e602a27302b8fe4af6861b09a08f5d
MobbareKurtZ <hannes.skoog@gmail.com>
2019-04-24 13:37:34 +0200

    Playing cover image fix

--- src/components/Bottom/Playing.vue
+++ src/components/Bottom/Playing.vue
@@ -1,7 +1,14 @@
 <template>
   <div v-if="currentPlayback" class="playing">
-    <h3>{{this.currentPlayback.item.name}}</h3>
-    <h4 v-for="artist in currentPlayback.item.artists" :key="artist.id">{{artist.name}}</h4>
+    <img
+      v-if="currentPlayback.item.album.images.length > 0"
+      :src="currentPlayback.item.album.images[1].url"
+      alt="cover"
+    >
+    <div class="info">
+      <h3>{{this.currentPlayback.item.name}}</h3>
+      <h4 v-for="artist in currentPlayback.item.artists" :key="artist.id">{{artist.name}}</h4>
+    </div>
   </div>
 </template>
 
